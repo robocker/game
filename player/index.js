fc3bd28fa85efa86adcfc2f37c969533bf29732f
Paweł Rymsza <pawel.rymsza@pastmo.pl>
2021-01-09 14:14:58 +0100

    Make request to engine

--- player/index.js
+++ player/index.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 const debug = require('debug')('player:index.js');
+const axios = require('axios');
 
-app.get('/', (req, res) => {
+app.get('/', async(req, res) => {
     debug('Fired hello world');
-    res.json({'msg':'Hello world!'});
+
+    const springMsg = await axios.get('http://localhost:8070/');
+
+    debug(springMsg.data);
+
+    res.json({msg:'Hello world!', fromSpring: springMsg.data});
 })
 
 app.listen(port, () => {
