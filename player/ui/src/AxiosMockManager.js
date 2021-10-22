319c6a7921af1166dd99fb07f36bdc69977911fe
Paweł Rymsza <pawel.rymsza@pastmo.pl>
2021-10-22 08:10:30 +0200

    Mocked response

--- player/ui/src/AxiosMockManager.js
+++ player/ui/src/AxiosMockManager.js
@@ -2,12 +2,55 @@ import axios from "axios";
 
 export class AxiosManager {
   static get(url, parameters) {
-      console.log('mock');
-    return {then:function(){return {}}};
+    console.log(`Mocked server response for: ${url}`);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        switch (url) {
+          case "api/info":
+            resolve({
+              data: {
+                tanks: [
+                  {
+                    containerName: "tank-1",
+                    height: 10,
+                    imageName: "robocker/tankbasic",
+                    insidePortNumber: 80,
+                    widthX: 5,
+                    widthY: 15,
+                    x: 105,
+                    y: 41,
+                  },
+                  {
+                    containerName: "tank-2",
+                    height: 10,
+                    imageName: "robocker/tankbasic",
+                    insidePortNumber: 80,
+                    widthX: 5,
+                    widthY: 15,
+                    x: 391,
+                    y: 426,
+                  },
+                ],
+              },
+            });
+            break;
+
+          default:
+            reject({ msg: `Not Mocked GET request: ${url}`, parameters });
+        }
+      }, 300);
+    });
   }
 
   static post(url, parameters) {
-    console.log('mock');
-    return {then:function(){return {}}};
+    console.log(`Mocked server response: ${url}`);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        switch (url) {
+          default:
+            reject({ msg: `Not Mocked POST request: ${url}`, parameters });
+        }
+      }, 300);
+    });
   }
 }
