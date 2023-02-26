77eaaeb823dd0830ad74ccaf95e8962aca4ab848
Paweł Rymsza <pawel.rymsza@pastmo.pl>
2023-02-26 21:15:57 +0100

    test_compute_angle_and_distance test, refactor

--- tankPython/app.py
+++ tankPython/app.py
@@ -15,25 +15,38 @@ def move():
     global current_tank, enemies
     destination = request.get_json()
 
-    # Find the closest enemy tank
     closest_enemy = get_closest_enemy(current_tank, enemies)
+    angle, distance = compute_angle_and_distance(current_tank, destination)
+    angle_to_enemy, vertical_angle_to_enemy = compute_aim(current_tank, closest_enemy, distance)
 
-    # Compute the angle and distance required to reach the destination
+    commands = create_commands(angle, distance, angle_to_enemy, vertical_angle_to_enemy)
+    send_message(ws, current_tank, commands)
+
+    return '', 204
+
+
+def compute_angle_and_distance(current_tank, destination):
     dx = destination['x'] - current_tank['x']
     dy = destination['y'] - current_tank['y']
     distance = math.sqrt(dx**2 + dy**2)
     angle = math.degrees(math.atan2(dy, dx))
+    if angle < 0:
+        angle = 360 - abs(angle)
+    return angle, distance
 
-    # Compute the angle and vertical angle required to aim at the closest enemy
-    dt = distance / 10  # time required for bullet to reach destination
-    ex = closest_enemy['x'] + closest_enemy['speedX'] * dt  # estimated x position of enemy at time of bullet impact
-    ey = closest_enemy['y'] + closest_enemy['speedY'] * dt  # estimated y position of enemy at time of bullet impact
-    etx = ex - current_tank['x']  # x distance from tank to estimated enemy position
-    ety = ey - current_tank['y']  # y distance from tank to estimated enemy position
-    angle_to_enemy = math.degrees(math.atan2(ety, etx))  # angle required to aim at estimated enemy position
-    vertical_angle_to_enemy = math.degrees(math.atan2(closest_enemy['z'] - current_tank['z'], distance))  # vertical angle required to aim at estimated enemy position
 
-    # Create the commands
+def compute_aim(current_tank, closest_enemy, distance):
+    dt = distance / 10
+    ex = closest_enemy['x'] + closest_enemy['speedX'] * dt
+    ey = closest_enemy['y'] + closest_enemy['speedY'] * dt
+    etx = ex - current_tank['x']
+    ety = ey - current_tank['y']
+    angle_to_enemy = math.degrees(math.atan2(ety, etx))
+    vertical_angle_to_enemy = math.degrees(math.atan2(closest_enemy['z'] - current_tank['z'], distance))
+    return angle_to_enemy, vertical_angle_to_enemy
+
+
+def create_commands(angle, distance, angle_to_enemy, vertical_angle_to_enemy):
     commands = [
         {
             'angle': angle,
@@ -47,34 +60,27 @@ def move():
             }
         }
     ]
+    return commands
 
-    # Send the message over the websocket
+
+def send_message(ws, current_tank, commands):
     message = {
         'tankId': current_tank['id'],
         'actions': commands
     }
     ws.send(json.dumps(message))
 
-    return '', 204
 
 def get_closest_enemy(current_tank, enemies):
-    # Initialize the closest enemy and its distance to be None and infinity, respectively
     closest_enemy = None
     closest_distance = float('inf')
-
-    # Iterate over all enemies to find the closest one
     for enemy in enemies:
-        # Calculate the distance between the current tank and the enemy
         dx = enemy['x'] - current_tank['x']
         dy = enemy['y'] - current_tank['y']
-        distance = math.sqrt(dx**2 + dy**2)
-
-        # If the current enemy is closer than the closest enemy, update the closest enemy and its distance
+        distance = math.sqrt(dx ** 2 + dy ** 2)
         if distance < closest_distance:
             closest_enemy = enemy
             closest_distance = distance
-
-    # Return the closest enemy
     return closest_enemy
 
 def retrieve_tank_info():
--- tankPython/app_test.py
+++ tankPython/app_test.py
@@ -1,8 +1,6 @@
-import pytest
-import json
-from unittest.mock import MagicMock
 from unittest.mock import patch
-from app import handle_websocket_message, retrieve_tank_info, get_closest_enemy, move, app
+from app import handle_websocket_message, retrieve_tank_info, get_closest_enemy, app,compute_angle_and_distance
+import math
 
 def test_handle_websocket_message():
     message = '{"tanks": [{"id": 1, "x": 10, "y": 20, "angle": 0, "playerId": 1, "lifeLevel": 100, "turret": {"angle": 0, "verticalAngle": 0}}]}'
@@ -114,39 +112,17 @@ def test_get_closest_enemy():
     assert get_closest_enemy(current_tank, enemies) is None
 
 
-from unittest.mock import MagicMock
+def test_compute_angle_and_distance():
+    # Test with destination at (0, 0)
+    current_tank = {'x': 1, 'y': 1}
+    destination = {'x': 0, 'y': 0}
+    angle, distance = compute_angle_and_distance(current_tank, destination)
+    assert math.isclose(angle, 315, rel_tol=1e-9) or math.isclose(angle, -45, rel_tol=1e-9)
+    assert math.isclose(distance, math.sqrt(2), rel_tol=1e-9)
 
-def test_move():
-    with app.test_client() as client:
-        destination = {"x": 10.0, "y": 10.0}
-
-        # Mock retrieve_tank_info function
-        with patch('app.retrieve_tank_info') as mock_retrieve_tank_info:
-            # Set current tank id and player id
-            current_tank = {"id": "tank-id-123", "playerId": "player-id-123"}
-            mock_retrieve_tank_info.return_value = current_tank
-
-            # Mock handle_websocket_message function
-            with patch('app.handle_websocket_message') as mock_handle_websocket_message:
-                # Send a POST request to /move endpoint with destination
-                response = client.post('/move', json=destination)
-                assert response.status_code == 200
-
-                # Verify websocket message is sent with correct data
-                mock_handle_websocket_message.assert_called_once_with(
-                    json.dumps({
-                        "tankId": "tank-id-123",
-                        "actions": [
-                            {"angle": 45.0},
-                            {"distance": 7.07},
-                            {
-                                "turret": {
-                                    "angle": 45.0,
-                                    "verticalAngle": 0.0,
-                                    "shoot": "END_OF_ACTION"
-                                }
-                            }
-                        ]
-                    }),
-                    current_tank
-                )
+    # Test with destination at (1, 2)
+    current_tank = {'x': 0, 'y': 0}
+    destination = {'x': 1, 'y': 2}
+    angle, distance = compute_angle_and_distance(current_tank, destination)
+    assert math.isclose(angle, 63.4349488, rel_tol=1e-9)
+    assert math.isclose(distance, math.sqrt(5), rel_tol=1e-9)
